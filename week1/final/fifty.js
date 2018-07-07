56ac579027d3fdf140601d456aba6dd4e0e73d41
Hongyuan Zhang <zhygwu2016@gmail.com>
2018-07-07 00:39:44 -0400

    20180706

--- week1/final/fifty.js
+++ week1/final/fifty.js
@@ -1,27 +1,27 @@
 // here we make a function called makeMarquee
 function makeMarquee() {
-  const title = 'FIFTY Music Festival — November 10–12, Desert Valley'
+  const title = 'FIFTY Music Festival — November 10–12, Desert Valley';
   // an array is a list of things
   // ['lawrence', 'rik', 'milan', 'ryan', 'adam', 'krista']
   // here we make a new empty array with 50 spaces in it
   // then we fill it with the text from our title (50 times)
   // we then join them all together as one text string using a ' — '
-  const marqueeText = new Array(50).fill(title).join(' — ')
+  const marqueeText = new Array(50).fill(title).join(' — ');
   // querySelector and querySelectorAll are the same as $ in jQuery
-  const marquee = document.querySelector('.marquee span')
+  const marquee = document.querySelector('.marquee span');
   // using innerHTML lets us set the content inside an element
-  marquee.innerHTML = marqueeText
+  marquee.innerHTML = marqueeText;
 }
 
 // here we actually run our makeMarquee function
-makeMarquee()
+makeMarquee();
 
 function random(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
 // here we grab all our the .circles from the html
-const circles = document.querySelectorAll('.circle')
+const circles = document.querySelectorAll('.circle');
 
 // circles returns us an array so we need to loop through it
 // inside of the forEach we get access to each individual element
@@ -74,7 +74,7 @@ inView('.section')
     section.classList.remove('in-viewport')
   })
 
-// here we set the class to add only once we have scrolled 0.2 of 
+// here we set the class to add only once we have scrolled 0.2 of
 // our section into the viewport
 inView.threshold(0.2)
 
@@ -91,19 +91,19 @@ sections.forEach((section, index) => {
 	// find elements inside of our section vs. our entire page
   const artists = section.querySelectorAll('.lineup li')
   const shapes = section.querySelectorAll('.shape')
-  
+
   artists.forEach((artist, index) => {
     const delay = index * 100
     artist.style.transitionDelay = delay + 'ms'
   })
-  
+
   shapes.forEach((shape, index) => {
 		// we are setting our delay up to only start once all of our
 		// artists have faded in, using the .length property
     const delay = (artists.length + index) * 100
     shape.style.transitionDelay = delay + 'ms'
   })
-  
+
 })
 
 // 1. whenever we click a .js-scroll link, we want to run a function
@@ -114,7 +114,7 @@ sections.forEach((section, index) => {
 const scrollLinks = document.querySelectorAll('.js-scroll')
 
 scrollLinks.forEach(link => {
-	
+
 	// addEventListener is just the same as jQuery’s .on()
   // we can listen for events on elements and then run a function
   link.addEventListener('click', (event) => {
@@ -123,28 +123,18 @@ scrollLinks.forEach(link => {
 
 		// this is equivalent to return false in jQuery
 		// it will block the default browser behaviour of the link jumping to
-		// the href attribute     
+		// the href attribute
     event.preventDefault()
-		
+
 		// here we grab the href attribute from our link
     const href = link.getAttribute('href')
     console.log(href)
-		// here we use the new  scrollIntoView feature to scroll to 
-		// our desired element in a smooth fashion    
-    document.querySelector(href).scrollIntoView({ 
-      behavior: 'smooth' 
+		// here we use the new  scrollIntoView feature to scroll to
+		// our desired element in a smooth fashion
+    document.querySelector(href).scrollIntoView({
+      behavior: 'smooth'
     })
 
   })
-  
+
 })
-
-
-
-
-
-
-
-
-
-
---
+++ week1/script.js
@@ -0,0 +1,98 @@
+function makeMarquee(){
+  const title = 'FIFTY Music Festival — November 10–12, Desert Valley';
+
+  // an array is a list of things
+
+  // here we make a new empty array with 50 spaces in it
+  // then we fill it with the text from our title (50 times)
+  // we then join them all together as one text string using a ' — '
+  const marqueeText = new Array(50).fill(title).join(' — ');
+
+  // 1. we want to grab our .marquee span form the html
+  // 2. we want to set our repeating title as the content
+  // !!! querySelector and querySelectorAll are the same as $ in jQuery
+  const marquee = document.querySelector('.marquee span');
+  marquee.innerHTML = marqueeText;
+  //$('.marquee span').html(marqueeText);
+  console.log(marquee);
+}
+
+// here we actually run our makeMarquee function
+makeMarquee();
+
+
+// circles animation using js
+// https://developer.mozilla.org/en-US/docs/Web/API/Element/animate
+// here we grab all our the .circles from the html
+const circles = document.querySelectorAll('.circle');
+// circles returns us an array so we need to loop through it
+// inside of the forEach we get access to each individual element
+// along with its index
+circles.forEach(function(circle, index){
+  // in here we get access to each one as 'circle'
+  //console.log(circle);
+  circle.animate([
+    // keyframes
+    { transform: 'scale(1)' },
+    { transform: 'scale(1.2)' },
+    { transform: 'scale(1)' },
+  ], {
+    // timing options
+    delay: 300 * index,
+    // here we use the index to create a staggered animation delay
+    duration: 3000,
+    iterations: Infinity
+  });
+});
+
+// 上面的初步原型：
+// document.querySelector(".shape").animate([
+//   // keyframes
+//   { transform: 'scale(1)' },
+//   { transform: 'scale(1.2)' },
+//   { transform: 'scale(1)' },
+// ], {
+//   // timing options
+//   duration: 3000,
+//   iterations: Infinity
+// });
+
+// 解释：
+// $('button').html('hello');与document.querySelectorAll('button')innerHtml('hello')有不同，后者只一次
+// right:
+//document.querySelectorAll('button').forEach(function(thing){
+//  thing.innerHTML('hello');
+//})
+//document.querySelectorAll('button').forEach(function(thing,index){
+//  console.log(index);
+//  thing.innerHTML('hello');
+//})
+
+
+// random function
+function random(min,max)
+{
+  return Math.floor(Math.random()*(max-min+1)+min);
+}
+
+
+// similar as .circle, the animation which makes .squiggle roate
+const squiggles = document.querySelectorAll('.squiggle');
+squiggles.forEach(function(squiggle, index){
+  // gets a random number between 0 and 45 using the random function
+  const randomNumber = random(0,45);
+  // create a bit of randomness for our animation delay
+
+  squiggle.animate([
+    // keyframes
+    { transform: 'rotate(0)' },
+    // here we join random number into our rotate property
+    { transform: 'rotate(' + randomNumber + 'deg)' },
+    { transform: 'rotate(0)' },
+  ], {
+    // timing options
+    delay: 300 * index,
+    duration: 5000,
+    iterations: Infinity
+  });
+});
